1cb89bf42c91fb5a603bbe46981f849ee083a2bf
jteng2127 <jteng2127@gmail.com>
2021-07-13 19:09:51 +0800

    change to vue2, add pages

--- src/main.js
+++ src/main.js
@@ -1,4 +1,31 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue from 'vue'
+import routes from './routes.js'
 
-createApp(App).mount('#app')
+Vue.config.productionTip = false
+
+const app = new Vue({
+  el: '#app',
+  data: {
+    currentRoute: window.location.pathname
+  },
+  computed: {
+    ViewComponent () {
+      console.log(this.currentRoute)
+      const matchingView = routes[this.currentRoute]
+      return matchingView
+        ? require('./pages/' + matchingView + '.vue')
+        : require('./pages/404.vue')
+    }
+  },
+  render (h) {
+    return h(this.ViewComponent)
+  }
+})
+
+window.addEventListener('popstate', () => {
+  app.currentRoute = window.location.pathname
+})
+
+//new Vue({
+//  render: h => h(App),
+//}).$mount('#app')
---
+++ src/pages/Home.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <p>Welcome home</p>
+  </div>
+</template>
