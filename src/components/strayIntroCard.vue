ee17dde73352e4b19fa10d1d7990d78a503e9c7c
jteng2127 <jteng2127@gmail.com>
2021-07-14 20:43:03 +0800

    update

---
+++ src/components/strayIntroCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="StrayIntroCard">
+    <div class="card text-center">
+      <img v-bind:src="require('@/assets/img/'+imgsrc)" v-bind:alt="imgsrc"/>
+      <div class="card-body">
+        <div class="card-title">{{ description }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StrayIntroCard',
+  props: {
+    imgsrc: String,
+    description: String
+  }
+}
+</script>
+
