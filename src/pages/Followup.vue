d26bfb089df3cbe2366a52c011305d30bf6beab1
jteng2127 <jteng2127@gmail.com>
2021-07-15 03:57:17 +0800

    update followup page

--- src/pages/Followup.vue
+++ src/pages/Followup.vue
@@ -8,16 +8,26 @@
       <b-button pill variant="primary" class="pos-abs s50">6/21</b-button>
       <b-button pill variant="light" class="pos-abs s100">6/30</b-button>
     </div>
-    <b-card title="Card title" sub-title="Card subtitle">
-      <b-card-text>
-        Some quick example text to build on the <em>card title</em> and make up
-        the bulk of the card's content.
-      </b-card-text>
+    <b-card title="領養狀況回報"> 
+      <b-form @submit="onSubmit">
 
-      <b-card-text>A second paragraph of text in the card.</b-card-text>
+        <b-form-group id="input-group-2" label="內容回報:" label-for="input-2">
+          <b-form-textarea
+            id="input-2"
+            v-model="form.content"
+            placeholder="Enter content"
+            rows="6"
+            max-rows="6"
+            required
+          ></b-form-textarea>
+        </b-form-group>
 
-      <a href="#" class="card-link">Card link</a>
-      <b-link href="#" class="card-link">Another link</b-link>
+        <b-form-file v-model="form.image" class="mt-3" plain @change="onFileChange"></b-form-file>
+        <div id="imgpreview" class="mb-3">
+          <img v-if="form.imgurl" :src="form.imgurl" />
+        </div>
+        <b-button type="submit" variant="primary">Submit</b-button>
+      </b-form>
     </b-card>
   </div>
 </template>
@@ -29,8 +39,25 @@ export default {
     return {
       value: 50,
       max: 100,
+      form: {
+        content: '',
+        image: null,
+        imgurl: null,
+      },
     };
   },
+  methods: {
+    onSubmit(event) {
+      event.preventDefault()
+      alert(JSON.stringify(this.form))
+    },
+    onFileChange(e) {
+      console.log("file change");
+      const file = e.target.files[0];
+      this.form.imgurl = URL.createObjectURL(file);
+      console.log(this.form.imgurl);
+    }
+  }
 };
 </script>
 
@@ -60,4 +87,10 @@ export default {
 .s100 {
   left: 100%;
 }
+
+#imgpreview img{
+  max-width: 100%;
+  max-height: 500px;
+}
+
 </style>
